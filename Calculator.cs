abd96409c7a1595eee49d9a285f398006afba3cf
trias <trias.2001@students.amikom.ac.id>
2020-03-10 12:13:14 +0700

    Menambahkan fungsi main

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,14 @@ class Calculator
 {
 	static void main (string[] args)
 	{
-	}
+		int a=10;
+		int b=6;
+		Consule.WriteLine("Hasil Penambahan: {0}+{1}={2}", a, b, Penambahan(a, b));
+		Consule.WriteLine("Hasi Pengurangan: {0}-{1}={2}", a,b, Pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.RedKey);
+		}
 		static int Penambahan(int a, int b)
 		{
 			return a+b;
