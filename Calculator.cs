35cd72d7a49e7fa0027a8a741184ffc8551ad395
trias <trias.2001@students.amikom.ac.id>
2020-03-10 12:04:33 +0700

    Menambahkan fungsi Pengurangan

--- Calculator.cs
+++ Calculator.cs
@@ -3,4 +3,12 @@ class Calculator
 	static void main (string[] args)
 	{
 	}
+		static int Penambahan(int a, int b)
+		{
+			return a+b;
+		}
+		static int Pengurangan(int a, int b)
+		{
+			return a-b;
+		}
 	}
